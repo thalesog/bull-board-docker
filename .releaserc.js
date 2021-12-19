0de35b731f8e90917c7544d277ae019e896a884a
Thales Ogliari <thalesog@me.com>
2021-12-19 16:39:26 -0300

    feat: semantic-release on docker hub

--- .releaserc.js
+++ .releaserc.js
@@ -4,5 +4,11 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
+    [
+      'semantic-release-docker',
+      {
+        name: 'thalesog/bullboard',
+      },
+    ],
   ],
 };
